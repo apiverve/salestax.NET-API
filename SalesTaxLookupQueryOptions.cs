a20a161331dc7ca93f2c5da5d5e204dba3af4546
APIVerve Team <hello@apiverve.com>
2024-08-20 11:06:44 -0500

    Bug Fixes

---
+++ SalesTaxLookupQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class SalesTaxLookupQueryOptions {
+public string zip { get; set; }
+}
+
+}
