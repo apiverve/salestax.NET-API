ff38d1375c4ddd58710ec25160de3c2f00588a29
APIVerve Team <hello@apiverve.com>
2024-08-20 11:06:42 -0500

    Bug Fixes

--- SalesTaxLookupAPIClient.cs
+++ SalesTaxLookupAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, salestaxQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, SalesTaxLookupQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(salestaxQueryOptions options = null)
+        public ResponseObj Execute(SalesTaxLookupQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(salestaxQueryOptions options)
+        private string constructURL(SalesTaxLookupQueryOptions options)
         {
             string url = _apiEndpoint;
 
